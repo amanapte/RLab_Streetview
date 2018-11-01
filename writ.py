d39a8895154d9208e396d41f66c873a06bb3f93d
amanapte <36478299+amanapte@users.noreply.github.com>
2018-11-01 22:51:51 +0530

    Add files via upload

---
+++ writ.py
@@ -0,0 +1,26 @@
+from __future__ import division
+
+import math
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.preprocessing.image import Iterator
+from keras.utils.np_utils import to_categorical
+import keras.backend as K
+
+def save_image(o,r,c,i,di):
+    f_name="orig"+str(i)+".jpg"
+    os.chdir(di+"/Output/Orig")
+    cv2.imwrite(f_name,o)
+
+    f_name="rotated"+str(i)+".jpg"
+    os.chdir(di+"/Output/Rotated")
+    cv2.imwrite(f_name,r)
+
+    f_name="correct"+str(i)+".jpg"
+    os.chdir(di+"/Output/Corrected")
+    cv2.imwrite(f_name,c)
+    
+    os.chdir(di)
