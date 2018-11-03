6108091ee82144b52ab3a4ec412c35aab30bf0f1
amanapte <36478299+amanapte@users.noreply.github.com>
2018-11-03 17:46:38 +0530

    Update writ.py

--- writ.py
+++ writ.py
@@ -13,14 +13,17 @@ import keras.backend as K
 def save_image(o,r,c,i,di):
     f_name="orig"+str(i)+".jpg"
     os.chdir(di+"/Output/Orig")
+    o=cv2.cvtColor(o, cv2.COLOR_RGB2BGR)
     cv2.imwrite(f_name,o)
 
     f_name="rotated"+str(i)+".jpg"
     os.chdir(di+"/Output/Rotated")
+    r=cv2.cvtColor(r, cv2.COLOR_RGB2BGR)
     cv2.imwrite(f_name,r)
 
     f_name="correct"+str(i)+".jpg"
     os.chdir(di+"/Output/Corrected")
+    c=cv2.cvtColor(c, cv2.COLOR_RGB2BGR)
     cv2.imwrite(f_name,c)
     
     os.chdir(di)
